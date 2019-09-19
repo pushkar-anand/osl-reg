4bf1593721461b94fa8c8fa00ec05e0dbd9232e7
Pushkar Anand <anandpushkar088@gmail.com>
2019-09-19 18:06:31 +0530

    logging fix.

--- src/logger.php
+++ src/logger.php
@@ -1,37 +1,33 @@
 <?php
-require_once __DIR__. "/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 use Monolog\Handler\NativeMailerHandler;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$logger = null;
-
 function getLogger()
 {
-    global $logger;
 
     $logFileDebug = __DIR__ . "/../logs/debug.log";
     $logFileError = __DIR__ . "/../logs/error.log";
     $logFileInfo = __DIR__ . "/../logs/info.log";
 
-    if ($logger === null) {
-        $logger = new Logger('Codezi.app Logger');
+    $logger = new Logger('Codezi.app Logger');
 
-        try {
-            $logger->pushHandler(new StreamHandler($logFileDebug, Logger::DEBUG));
-            $logger->pushHandler(new StreamHandler($logFileError, Logger::ERROR));
-            $logger->pushHandler(new StreamHandler($logFileInfo, Logger::INFO));
-            $logger->pushHandler(new NativeMailerHandler(TO_EMAIL,
-                SUBJECT,
-                FROM_EMAIL,
-                Logger::CRITICAL));
+    try {
+        $logger->pushHandler(new StreamHandler($logFileDebug, Logger::DEBUG));
+        $logger->pushHandler(new StreamHandler($logFileError, Logger::ERROR));
+        $logger->pushHandler(new StreamHandler($logFileInfo, Logger::INFO));
+        $logger->pushHandler(new NativeMailerHandler(TO_EMAIL,
+            SUBJECT,
+            FROM_EMAIL,
+            Logger::CRITICAL));
 
-            $logger->info('Logger is now ready');
+        $logger->info('Logger is now ready');
 
-        } catch (Exception $e) {
-            error_log("Error in setting up logger." . $e->getMessage());
-        }
+    } catch (Exception $e) {
+        error_log("Error in setting up logger." . $e->getMessage());
     }
+
     return $logger;
 }
